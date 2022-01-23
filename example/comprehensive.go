6442a928d8783990ada0682e6140d6d63e8d736c
polarspetroll <polarspetroll@gmail.com>
2022-01-23 22:21:12 +0330

    add example

---
+++ example/comprehensive.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	lcd "github.com/polarspetroll/LiquidCrystalRPI"
+)
+
+func main() {
+	l, err := lcd.NewLCD(0x27, 16, 2) // specify the I²C device address, width and height
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* You can also use the default configuration like so :
+
+	l := lcd.DefaultLCD
+	*/
+	l.Print("Hello World!", 1) // print 'Hello World' at line 1
+	l.Print("Second Line", 2)  // print 'Second Line' at line 2
+	time.Sleep(3 * time.Second)
+	l.Clear() // clear the display
+	time.Sleep(3 * time.Second)
+	l.BackLightOff() // turn off backlight
+	time.Sleep(2 * time.Second)
+	l.BackLightOn() //turn on backlight
+}
---
+++ example/scroll.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	lcd "github.com/polarspetroll/LiquidCrystalRPI"
+)
+
+func main() {
+	l := lcd.DefaultLCD
+	delay := 300 * time.Millisecond
+	for i := 0; i < 2; i++ {
+		l.ScrollText("Long Text : github.com/polarspetroll/LiquidCrystalRPI", 1, delay)
+	}
+	l.Clear()
+	for i := 0; i < 2; i++ {
+		l.ScrollText("Short Text!", 1, delay)
+	}
+
+}
